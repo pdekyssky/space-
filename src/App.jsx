8af323015efe2120fd4ce800fef959840817470f
pdekyssky <p.dekyssky@gmail.com>
2023-01-27 17:09:05 +0100

    show // hide

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import "./App.css"
+import Book from "./components/Book"
 
 /*const App = () => {
 
@@ -52,7 +53,7 @@ import "./App.css"
 export default App*/
 
 
-const url = "http://api.open-notify.org/iss-now.json"
+/*const url = "http://api.open-notify.org/iss-now.json"
 
 const App = () => {
 
@@ -89,4 +90,40 @@ const App = () => {
     
 }
 
+export default App  */
+
+//short circuit evaluation
+
+/*const App = () => {
+
+const value = ""
+const value2 = ""
+const result1 = value && 555
+//console.log(result1);
+//if first condition  is true its return second atribute
+// if first condition is falser it is return first condition
+const result2 = value || "peter"
+console.log(result2)
+//if first condition is true  -- return first 
+//if first condition is false -- reutrn second
+    return (
+        <div>
+            <h2>{result1}</h2>
+        </div>
+    )
+}
+
+export default App*/
+
+const App = () => {
+
+const [show, setShow] = useState(true)
+
+    return (
+        <div>
+        <button onClick={() => setShow(!show)}>Ukaz / skry</button>
+           {show && <Book />}
+        </div>
+    )
+}
 export default App
---
+++ src/components/Book.jsx
@@ -0,0 +1,11 @@
+import "./Book.css"
+
+const Book = () => {
+    return (
+        <div>
+            <h1>Piesen ladu a ohna</h1>
+        </div>
+    )
+}
+
+export default Book
