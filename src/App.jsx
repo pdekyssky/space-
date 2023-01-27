ed3898eecae87f3a2df72b8ad8f67d0aa86cff14
pdekyssky <p.dekyssky@gmail.com>
2023-01-27 15:01:28 +0100

    multiple returns

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react"
 import "./App.css"
 
-const App = () => {
+/*const App = () => {
 
     //api
     const url = "http://api.open-notify.org/iss-now.json"
     
-    let [latitude, setLatitude] = useState("")
-    let [longitude, setLongitude] = useState("")
+    const [latitude, setLatitude] = useState("")
+    const [longitude, setLongitude] = useState("")
+    const [mapUrl, setMapUrl] = useState("")
    
     const getCoordinates = async() => {
         const response = await fetch(url) 
@@ -18,6 +19,11 @@ const App = () => {
        
         setLatitude(data["iss_position"]["latitude"]) 
         setLongitude(data["iss_position"]["longitude"])
+
+        const iss_long = data["iss_position"]["longitude"]
+        const iss_lat = data["iss_position"]["latitude"]
+
+        setMapUrl(`https://sk.mapy.cz/zakladni?x=${iss_long}&y=${iss_lat}&z=8`)
     }
 
     const handleClick = () => {
@@ -37,8 +43,50 @@ const App = () => {
             <h2>Zemepisna dlzka</h2>
             <p>{longitude}</p>
             <button onClick={handleClick}>New Cords</button>
+            <p>najskor klikni na new cords</p>
+            <a href={mapUrl} target="_blank">Pozicia ISS v mapach</a>
         </div>
     )
+}
+
+export default App*/
+
+
+const url = "http://api.open-notify.org/iss-now.json"
+
+const App = () => {
+
+    const [loading, setLoading] = useState(true)
+    const [latitude, setLatitude] = useState("")
+    const [longitude, setLongitude] = useState("")
+
+    useEffect( () => {
+        fetch(url)
+            .then( (response) => response.json() )
+            .then( (data) => data["iss_position"])
+            .then( (position) => {
+                
+                console.log(position["latitude"]);
+                console.log(position["longitude"]);
+                
+                setLatitude(position["latitude"])
+                setLongitude(position["longitude"])
+            })
+            setLoading(false)
+    }, [] )
+
+    if (loading) {
+        return <h2>Loading</h2>
+    } 
+        return (
+            <div>
+                <h2>Zemepisna sirka</h2>
+                <p>{latitude}</p>
+                <h2>Zemepisna dlzka</h2>
+                <p>{longitude}</p>
+            </div>
+        )
+    
 }
 
 export default App
